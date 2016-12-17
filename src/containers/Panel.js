545546bd90d37772b572d9b4b32a795902c4e26e
Mosoc Chen <mosoc@icloud.com>
2016-12-17 15:06:49 +0900

    split components and containers

---
+++ src/containers/Panel.js
@@ -0,0 +1,32 @@
+import React from 'react';
+// import components
+import Show from '../components/Show';
+import Btn from '../components/Btn';
+
+
+class Panel extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+        number: 0
+    }
+  }
+
+  increment = () => {
+    let newState = {
+      number: this.state.number + 1
+    }
+
+    this.setState(newState);
+  }
+
+  render(){
+    return(
+        <div>
+          <Show num={this.state.number}/>
+          <Btn increment={this.increment}/>
+        </div>
+    )
+  }
+}
+export default Panel;
--- src/main.js
+++ src/main.js
@@ -1,54 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Panel extends React.Component{
-  constructor(){
-    super();
-    this.state = {
-        number: 0
-    }
-  }
-
-  increment = () => {
-    let newState = {
-      number: this.state.number + 1
-    }
-
-    this.setState(newState);
-  }
-
-  render(){
-    return(
-        <div>
-          <Show num={this.state.number}/>
-          <Btn increment={this.increment}/>
-        </div>
-    )
-  }
-}
-
-
-class Show extends React.Component{
-  render(){
-    const  { num }= this.props;
-
-    return (
-      <div>{ num }</div>
-    )
-  }
-}
-
-class Btn extends React.Component {
-  render(){
-    const { increment} = this.props;
-
-    return(
-      <div>
-        <button onClick = {increment}> + </button>
-      </div>
-    )
-  }
-}
-
+// import container
+import Panel from './containers/Panel';
 
 ReactDOM.render(<Panel />, document.getElementById('app'));
