37ddf1938dcbf7f47425ad8a31a9f076617c5728
Bruno Anderson Chen <mosoc@colle.co>
2019-09-06 13:03:41 +0900

    Upgrade webpackes packages with configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,13 @@
 var path = require('path');
 module.exports = {
-    entry: ['webpack/hot/dev-server', path.resolve(__dirname, 'src/main.js')],
-    output: {
-        filename: './dist/bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loaders: ['react-hot', 'babel']
-            }
-        ]
-    }, devtool: 'source-map'
+  entry: ['webpack/hot/dev-server', path.resolve(__dirname, 'src/main.js')],
+  output: {
+    filename: './dist/bundle.js'
+  },
+  module: {
+    rules: [{
+      loaders: 'babel-loader'
+    }]
+  },
+  devtool: 'source-map'
 };
